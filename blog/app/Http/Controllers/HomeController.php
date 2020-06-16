587af67bb9d76a2fa6a428f89e842920add7172f
MarcoMadeddu <marcomade99@gmail.com>
2020-06-16 18:50:18 +0200

    complete

---
+++ blog/app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        //ottieni tutti i record da una tabella 
+        // $students = Student::all();
+        // dd($students);
+
+        // WHERE
+        // $students = student::where('nome' , 'marco madeddu' )->get();
+
+        // ORDER BY (LIMIT)
+        // $students = Student::orderBy('nome', 'asc') ->limit(1)->get();
+
+         // ORDER BY
+        // $students = Student::orderBy('nome', 'asc')->get();
+
+        //first
+        $students = Student::where('id' , 1)->first();
+
+        return view('welcome' , compact('students'));
+    }
+}
